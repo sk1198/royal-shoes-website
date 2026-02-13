92345afec92102fae5f9ae9c8c56cfa63f57406f
krunal shah <krunalshah119@gmail.com>
2026-02-14 02:07:51 +0530

    phase2 SPA navigation added

--- js/script.js
+++ js/script.js
@@ -6,3 +6,33 @@ i++;
 if(i>=images.length){i=0;}
 document.getElementById("slide").src = images[i];
 },3000);
+
+function showSection(sectionId, el){
+  let pages = document.querySelectorAll(".page");
+  pages.forEach(page => {
+    page.style.display = "none";
+    page.classList.remove("fade");
+  });
+
+  document.getElementById(sectionId).style.display = "block";
+  document.getElementById(sectionId).classList.add("fade");
+
+//active menu
+let links = document.querySelectorAll(".navlink");
+links.forEach(l=>classList.remove("active"));
+el.classList.add("active");
+}
+
+window.onload=function(){
+document.getElementById("home").style.display="block";
+};
+function loadFromHash(){
+let hash=window.location.hash.replace("#","") || "home";
+let link=document.querySelector(`a[href="#${hash}"]`);
+if(link){
+showSection(hash,link);
+}
+}
+
+window.addEventListener("hashchange",loadFromHash);
+window.onload=loadFromHash;
